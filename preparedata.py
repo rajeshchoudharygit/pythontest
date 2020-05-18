d57e1ef7bf9fcf94f896e45df479452b764ab8ee
Rajeshchoudhary-Python <rajesh.choudhary201415@gmail.com>
2020-05-18 10:01:55 +0200

    defined task2 function, defined test case and added function to main

--- preparedata.py
+++ preparedata.py
@@ -7,3 +7,19 @@ def task1(tenant_data):
     print(Current_Rent[:5])
     print('\n')
     return Current_Rent[:5]
+
+def task2(tenant_data):
+    """create a new list of mast data with “Lease Years” = 25 years"""
+    lease_years_25 = [item for item in tenant_data if item['Lease Years'] == '25']
+    # calculating the total rent for all tenants with 25 years lease using List Comprehension
+    total_rent = sum([float(item['Current Rent']) for item in tenant_data if item['Lease Years'] == '25'])
+    count = 1
+    #displaying the list of tenants with 25 years lease, inlcuding all data fields
+    for each_dict in lease_years_25:
+        print(f'Details of tenant number {count} with lease for 25 years \n')
+        for key, value in each_dict.items():
+            print(key + ' : ' + value)
+        count += 1
+    #total rent for all tenansts with 25 years lease
+    print(f"\nThe total rent for all tenants with 25 years lease is {total_rent}\n")
+    return total_rent
