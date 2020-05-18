a2e574d8f09ebb758edc5b46f5c4fed74fbb082c
Rajeshchoudhary-Python <rajesh.choudhary201415@gmail.com>
2020-05-18 07:48:25 +0200

    adding main file and readscv, readcsv reads test data from csv file"

---
+++ main.py
@@ -0,0 +1,4 @@
+from readcsv import read_csv
+
+if __name__ == '__main__':
+    customer_data = read_csv()
---
+++ readcsv.py
@@ -0,0 +1,14 @@
+import csv
+
+
+def read_csv():
+    """read data from csv file, make a list of dictionary from the data and return list to the caller"""
+    csv.register_dialect('myDialect',
+                         delimiter=',',
+                         quoting=csv.QUOTE_ALL)
+    customer_data = []
+    with open('./testdata/Python Developer Test Dataset.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file,  dialect='myDialect')
+        for row in csv_reader:
+            customer_data.append(row)
+    return customer_data
