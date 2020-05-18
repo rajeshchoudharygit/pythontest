a827f38e6a481d401cf327611ed6037907fb264f
Rajeshchoudhary-Python <rajesh.choudhary201415@gmail.com>
2020-05-18 10:01:01 +0200

    added callto task2 function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from readcsv import read_csv
-from preparedata import task1
+from preparedata import task1, task2
 
 if __name__ == '__main__':
     tenant_data = read_csv()
-    print(tenant_data)
-    task1(tenant_data)
+    #task1(tenant_data)
+    task2(tenant_data)
+
